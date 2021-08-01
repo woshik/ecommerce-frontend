e2ad0edd74d61a8863cff0bb2ec0bbe20f548377
anik <anik@w3engineers.com>
2021-08-02 01:04:36 +0600

    # Feature  - eslint enable and configure for application  - husky configuration complete  - modify gitignore file, ignore all .env files

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tx', '.tsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': ['error', 'ignorePackages', { ts: 'never', tsx: 'never' }],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+    'react/prop-types': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-function App() {
-  return (
-    <h1>E-commerce</h1>
-  );
-}
+import { FC } from 'react';
+
+const App: FC = () => (
+  <h1>E-commerce</h1>
+);
 
 export default App;
